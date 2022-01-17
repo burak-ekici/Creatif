5c57ac3ed99af846028b15441da0674a4985e399
Ekici-Burak <b.ekici@it-students.fr>
2022-01-17 12:20:14 +0100

    17/01/22 creatif webdesign

--- generativeArt/bubbleText/index.js
+++ generativeArt/bubbleText/index.js
@@ -16,6 +16,7 @@ const mouse = {
 window.addEventListener('mousemove',(e)=>{
   mouse.x = e.x
   mouse.y = e.y
+  mouse.radius = 150
 })
 
 // window.addEventListener('resize', (e)=>{
@@ -25,8 +26,8 @@ window.addEventListener('mousemove',(e)=>{
 
 ctx.fillStyle = 'white'
 ctx.font = '30px Verdana'
-ctx.fillText('A', 0 , 30)
-const data = ctx.getImageData(0,0,100,100)
+ctx.fillText('Salut', 0 , 30)
+const textCoordinates = ctx.getImageData(0,0,100,100)
 
 class Particule{
 
@@ -36,10 +37,10 @@ class Particule{
     this.size = 3;
     this.baseX = this.x;
     this.baseY = this.y;
-    this.density = (Math.random() * 30) + 1
+    this.density = (Math.random() * 20) + 5
   } 
   draw(){
-    ctx.fillStyle = 'red';
+    ctx.fillStyle = 'white';
     ctx.beginPath()
     ctx.arc(this.x,this.y,this.size,0, Math.PI *2)
     ctx.closePath()
@@ -48,21 +49,40 @@ class Particule{
   update(){
     let dx = mouse.x - this.x;
     let dy = mouse.y - this.y
-    let distance = Math.sqrt(dx * dx + dy * dy)
-    if(distance < 100){
-      this.size = 20
+    let distance = Math.sqrt(dx * dx + dy * dy) // hypotenus
+    let forceDirectionX = dx / distance
+    let forceDirectionY = dy / distance
+    let maxDistance = mouse.radius
+    let force = (maxDistance - distance) / maxDistance
+    let directionX = forceDirectionX * force * this.density
+    let directionY = forceDirectionY * force * this.density
+    if(distance < mouse.radius){   //rayon d'action de la souris
+      this.x -= directionX
+      this.y -= directionY
     }else{
-      this.size = 3;
+      if(this.x !== this.baseX){
+        let dx = this.x - this.baseX
+        this.x -= dx / 5
+      }
+      if(this.y !== this.baseY){
+        let dy = this.y - this.baseY
+        this.y -= dy / 5
+      }
     }
   }
 }
 
 function init(){
   particleArray = [];
-  for (let i = 0 ; i < 500 ; i++){
-    let x = Math.random() * canvas.width;
-    let y = Math.random() * canvas.height;
-    particleArray.push(new Particule(x,y))
+  console.log(textCoordinates)
+  for ( let y = 0 ,  y2 = textCoordinates.height ; y < y2 ; y++){
+    for(let x = 0 , x2 = textCoordinates.width ; x < x2 ; x++){
+      if(textCoordinates.data[(y * 4 * textCoordinates.width) + ( x * 4) + 3] > 128){
+        let positionX = x;
+        let positionY = y;
+        particleArray.push(new Particule(positionX * 10 + 100, positionY * 10 + 100))
+      }
+    }
   }
 }
 
@@ -76,6 +96,29 @@ function animate(){
     particleArray[i].draw();
     particleArray[i].update();
   }
+  connectParticle()
   requestAnimationFrame(animate);
 }
 animate();
+
+function connectParticle(){
+  let opacityValue = 1
+  for(let a = 0 ; a < particleArray.length ; a++){
+    for(let b = a ; b < particleArray.length ; b++){
+      let dx = particleArray[a].x - particleArray[b].x
+      let dy = particleArray[a].y - particleArray[b].y
+      let distance = Math.sqrt(dx * dx + dy * dy)
+      
+      if(distance < 30){
+        opacityValue = 1 - (distance/30)
+        ctx.strokeStyle = `rgba(255,255,255,${opacityValue})`;
+        ctx.lineWidth = 2;
+        ctx.beginPath();
+        ctx.moveTo(particleArray[a].x , particleArray[a].y)
+        ctx.lineTo(particleArray[b].x , particleArray[b].y)
+        ctx.stroke()
+      }
+      
+    }
+  }
+}
